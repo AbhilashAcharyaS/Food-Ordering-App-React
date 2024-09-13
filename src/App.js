005c9da147580892b4d81d3e3b9f0bb4425dc1cc
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2024-09-13 12:58:18 +0530

    Using swiggy api to render restaurants

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-import RestaurantCard from "./components/RestaurantCard";
 
 const AppLayout = () => {
   return (
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,26 @@
 import RestaurantCard from "./RestaurantCard";
-import restaurantList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
 
-  const [listOfRestaurants , setListOfRestaurants] = useState(restaurantList);
+  const [listOfRestaurants , setListOfRestaurants] = useState([]);
+
+  useEffect(()=>{
+    fetchData()
+  },[]);
+
+  const fetchData = async ()=>{
+    const data= await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+
+    const jsonData= await data.json();
+    console.log(jsonData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+
+    setListOfRestaurants(jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }
+  
+  if(listOfRestaurants.length===0){
+    return <h1 style={{textAlign:"center"}}>Loading...</h1>
+  }
 
     return (
       <div className="body">
@@ -15,6 +31,17 @@ const Body = () => {
               console.log(filteredResList);
               setListOfRestaurants(filteredResList);
             }}>Top rated Restarants</button>
+
+
+          {/* <button className="filter-btn" 
+            onClick={()=>{
+              let sortedResList= listOfRestaurants.toSorted((a, b) => a.info.sla.deliveryTime < b.info.sla.deliveryTime ? -1 : 1);
+              console.log(sortedResList);
+              setListOfRestaurants(sortedResList);
+            }}>Sort based on delivery time</button> */}
+
+            
+            
         </div>
         <div className="res-container">
           {listOfRestaurants.map((res) => (
