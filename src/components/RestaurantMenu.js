69459293d6988834338561d1806c253199b215e2
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2024-09-18 22:05:42 +0530

    custom hook for Restaurant menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,24 +1,12 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Shimmer from "./Shimmer";
-import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
+
 
 const RestaurantMenu =()=>{
 
-    const [resInfo, setResInfo] = useState(null);
-
-    useEffect(()=>{
-        fetchData()
-    },[]);
-
     const {resId} = useParams();
-  
-    const fetchData= async ()=>{
-        const data= await fetch(MENU_API+resId);
-        const json= await data.json();
-        console.log(json.data);
-        setResInfo(json.data);
-    }
+    const resInfo = useRestaurantMenu(resId);
 
     if(resInfo === null) return <Shimmer/>;
 
@@ -32,7 +20,7 @@ const RestaurantMenu =()=>{
             <div className="res-details">
             <h2>{name}</h2>
             <h4>{avgRating} <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
-  <path fillRule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clipRule="evenodd" />
+            <path fillRule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clipRule="evenodd" />
 </svg>
 
 ({totalRatingsString})  : {costForTwoMessage}</h4>
