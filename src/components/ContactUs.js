9e278bb898f27cbad10a735cdcabe2f43db702ed
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2024-10-02 23:12:29 +0530

    Contavt us page

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,7 +2,14 @@
 const ContactUs = ()=>{
     return (
         <div className="body">
-            <h1 className="text-lg font-bold text-center">Ways to contact Us</h1>
+            <h1 className="text-lg font-bold text-center">Contact Us</h1>
+            <form className="w-1/3 mx-auto">
+                <input type="text" placeholder="Name" className=" w-full border border-black p-2 my-2 rounded-lg"></input>
+                <br></br>
+                <textarea type="text" placeholder="Message" rows={3} className="w-full border p-2 my-2 border-black rounded-lg"></textarea>
+                <br></br>
+                <button className="w-full border border-black p-2 rounded-lg bg-blue-200 hover:font-semibold hover:bg-blue-400">Submit</button>
+            </form>
         </div>
     )
 }
