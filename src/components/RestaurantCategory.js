2e4ec41a69121e6783d83720e86540199484b4e1
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2024-10-03 23:24:22 +0530

    testing restaurant card with props data using mock data

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -11,8 +11,8 @@ const RestaurantCategory = ({data, showItems, setShowIndex})=>{
             <div className="accordion-header flex justify-between items-center my-4 shadow-md      bg-white  py-4 px-2 cursor-pointer" onClick={handleClick}>
               <h1 className="font-bold text-lg">{data.title} ({data.itemCards.length})</h1>
                <span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"       stroke-width="1.5" stroke="currentColor" className="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"       strokeWidth="1.5" stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                 </svg>
                 
                 </span>
